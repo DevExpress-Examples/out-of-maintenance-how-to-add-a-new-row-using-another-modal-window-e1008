5e9a10260ca778b0e106ea3ce90c1e29e735aa81
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CreateNewRowWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace AddNewRow {
+    public partial class CreateNewRowWindow : Window {
+        public CreateNewRowWindow() {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
