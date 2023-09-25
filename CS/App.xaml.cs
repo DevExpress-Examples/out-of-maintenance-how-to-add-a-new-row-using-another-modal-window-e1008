030ddaa5ca11f7649c82691b4cb53f12ccdf359d
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 15:35:40 +0400

    Change example status to out of maintenance

--- CS/App.xaml.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Windows;
-
-namespace AddNewRow {
-
-    public partial class App : Application {
-    }
-}
--- VB/App.xaml.vb
+++
@@ -1,9 +0,0 @@
-Imports System.Windows
-
-Namespace AddNewRow
-
-    Public Partial Class App
-        Inherits Application
-
-    End Class
-End Namespace
